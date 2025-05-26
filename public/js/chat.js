c4b3d15a48d07eae163155eec7a7ee59faccc0cb
Dali Duraku <mesherbetmolla@gmail.com>
2025-05-26 18:16:24 -0400

    feat: rendering location messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,7 +11,8 @@ const $sendLocationButton = document.getElementById('send-location');
 const $messages = document.querySelector('#messages');
 
 // Templates
-const messageTemplate = document.querySelector('#message-template').innerHTML
+const messageTemplate = document.querySelector('#message-template').innerHTML;
+const urlTemplate = document.querySelector('#url-template').innerHTML;
 
 
 socket.on('message', (message) => {
@@ -20,6 +21,14 @@ socket.on('message', (message) => {
         message
     });
     $messages.insertAdjacentHTML('beforeend', html);
+})
+
+socket.on('locationMessage', url => {
+   console.log(url)
+   const html = Mustache.render(urlTemplate, {
+    url
+   });
+   $messages.insertAdjacentHTML('beforeend', html)
 })
 
 $messageForm.addEventListener('submit', (e) => {
