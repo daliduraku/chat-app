4f1c0084fe151dc7714ce97f15edd317bfbe8428
Dali Duraku <mesherbetmolla@gmail.com>
2025-05-27 13:38:12 -0400

    feat: sending messages to rooms

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Server } from 'socket.io';
 import { fileURLToPath } from 'url';
 import { Filter } from 'bad-words'
 import { generateMessage, generateLocationMessage } from './utils/messages.js';
+import { addUser, removeUser, getUser, getUsersInRoom } from './utils/users.js';
 
 
 const app = express();
@@ -25,22 +26,33 @@ io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
 
-    socket.on('join', ({ username, room }) => {
-        socket.join(room)
+    socket.on('join', ({ username, room }, callback) => {
+
+        const { error, user } = addUser({ id: socket.id, username, room });
+
+        if(error) {
+            return callback(error)
+        }
+
+        socket.join(user.room)
 
         socket.emit('message', generateMessage('Welcome!'));
-        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`))
+        socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`));
+        callback()
+
         // socket.emit, io.emit, socket.broadcast.emit
         // io.to.emit, socket.broadcast.to.emit
     })
 
     socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
         const filter = new Filter()
         if(filter.isProfane(message)) {
             return callback('Profanity is not allowed!')
         }
 
-        io.to('brooklyn').emit('message', generateMessage(message));
+        io.to(user.room).emit('message', generateMessage(message));
         callback('Delivered!')
     })
     
@@ -51,7 +63,14 @@ io.on('connection', (socket) => {
     })
 
     socket.on('disconnect', () => {
-        io.emit('message', generateMessage('A user has left!'))
+        const user = removeUser(socket.id);
+
+        if(user) {
+            io.to(user.room).emit('message', generateMessage(`${user.username} has left!`))
+        }
+
+
+
     })
 
 })
